a81304cffcaeec26f5e863b53834e30540742e39
Andrew <Piggby@users.noreply.github.com>
2019-04-26 16:32:46 -0700

    monster command
    h

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const token = process.env.token;
 var fs = require("fs");
 var adjectives = JSON.parse(fs.readFileSync("adjectives.json"));
 var items = JSON.parse(fs.readFileSync("items.json"));
+var monsters = JSON.parse(fs.readFileSync("monsters.json"));
 
 
 client.once('ready', () => {
@@ -15,9 +16,9 @@ client.once('ready', () => {
 
 client.on('message', message => {
   //console.log(message.content);
-  client.user.setActivity('.idea | .ideas | .ping')
-  if (message.content.startsWith(`${prefix}ideas`)) message.channel.send('<@' + message.author.id + '> | **' +adjectives[Math.floor(Math.random()*adjectives.length)] + " " + adjectives[Math.floor(Math.random()*adjectives.length)] + " " + items[Math.floor(Math.random()*items.length )] + "**")
-  else if(message.content.startsWith(`${prefix}idea`)) message.channel.send('<@' + message.author.id + '> | **' + adjectives[Math.floor(Math.random()*adjectives.length)] + " " + items[Math.floor(Math.random()*items.length)] + "**")
+  client.user.setActivity('.item | .monster | .ping')
+  if (message.content.startsWith(`${prefix}monster`)) message.channel.send('<@' + message.author.id + '> | **' + adjectives[Math.floor(Math.random()*adjectives.length)] + " " + monsters[Math.floor(Math.random()*monsters.length)] + "**")
+  else if(message.content.startsWith(`${prefix}item`)) message.channel.send('<@' + message.author.id + '> | **' + adjectives[Math.floor(Math.random()*adjectives.length)] + " " + items[Math.floor(Math.random()*items.length)] + "**")
   else if (message.content === `${prefix}ping`) message.channel.send('<@' + message.author.id + '> | **' + client.ping + " ms**");
 })
 
