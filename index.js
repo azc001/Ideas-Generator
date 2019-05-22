fdb86f3ec4a512df01b019efad938c0cf8bcb6f8
Andrew <andrzchen@gmail.com>
2019-05-21 22:21:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ client.on('message', message => {
               .then(collected => {
                 const reaction = collected.first();
                 if (reaction.emoji.name === '🗡') {
-                  while (prompt === '🔄') {
+                  do {
                     if (Math.random() >= 0.5) var gen1 = items[Math.floor(Math.random()*items.length)] + " of the " + monsters[Math.floor(Math.random()*monsters.length)];
                     else var gen1 = adjectives[Math.floor(Math.random()*adjectives.length)] + " " + items[Math.floor(Math.random()*items.length)];
                     if (Math.random() >= 0.5) var gen2 = items[Math.floor(Math.random()*items.length)] + " of the " + monsters[Math.floor(Math.random()*monsters.length)];
@@ -86,8 +86,9 @@ client.on('message', message => {
                       });
                   });
                 }
+                while (prompt === '🔄')
                 }  else if (reaction.emoji.name === '👹') {
-                  while (prompt === '🔄') {
+                  do {
                     if (Math.random() >= 0.5) var gen1 = adjectives[Math.floor(Math.random()*adjectives.length)] + " " + monsters[Math.floor(Math.random()*monsters.length)];
                     else var gen1 =  "The " + adjectives[Math.floor(Math.random()*adjectives.length)] + " " + dungeons[Math.floor(Math.random()*dungeons.length)];
                     if (Math.random() >= 0.5) var gen2 = adjectives[Math.floor(Math.random()*adjectives.length)] + " " + monsters[Math.floor(Math.random()*monsters.length)];
@@ -121,6 +122,7 @@ client.on('message', message => {
                       });
                   });
                 }
+                while (prompt === '🔄')
                 }
               })
               .catch(collected => {
